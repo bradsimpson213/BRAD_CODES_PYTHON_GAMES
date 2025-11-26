ef76a449581c7cf985e5c9c62344d77725ceb38d
Brad Simpson <bradsimpson@icloud.com>
2025-11-26 14:07:15 -0500

    added tictactoe constructor

--- 04_Tic_Tac_Toe/solution/tic_tac_toe.py
+++ 04_Tic_Tac_Toe/solution/tic_tac_toe.py
@@ -1,9 +1,21 @@
 import os
+from time import sleep
+from logo import logo, instructions
+from display import print_display
 
 
 class TicTacToe:
+    """Class that creates instances of the Tic Tac Toe game"""
+
     def __init__(self):
-        pass
+        """Constructor to set up the game"""
+        self.board = [" ", " ", " ", 
+                      " ", " ", " ", 
+                      " ", " ", " "]
+        self.player1 = None
+        self.player2 = None
+        self.player_turn = 1
+        self.play_game()
 
 
     def set_player_type(self):
